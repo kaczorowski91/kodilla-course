826f90827775aa2ea855cbb00ba0c3b6d7b500b2
kaczorowski91 <obsadalzpr@gmail.com>
2019-11-07 11:50:50 +0100

    version 0.6 remove private variables

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,7 +4,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        //    System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
 
         Calculator calculator1 = new Calculator();
 
