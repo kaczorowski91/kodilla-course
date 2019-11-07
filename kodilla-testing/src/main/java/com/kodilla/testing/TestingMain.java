5444477ed1a7f4f12326b9437bf18e6ae3c1e154
kaczorowski91 <obsadalzpr@gmail.com>
2019-11-07 09:47:16 +0100

    test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        //    System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator1 = new Calculator(5, 8);
+
+        if(calculator1.add() == 13) {
+            System.out.println("Adding is OK");
+        }else{
+            System.out.println("Adding has Error");
+        }
+
+        if(calculator1.subtraction() == -3) {
+            System.out.println("Subtractionningn is OK");
+        }else{
+            System.out.println("Subtractionning has Error");
+        }
     }
 }
