0674688a7d7552b6de8691d9a0819ffe2be191cb
kaczorowski91@gmail.com <kaczorowski91@gmail.com>
2019-11-13 11:53:47 +0100

    task #7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
@@ -45,5 +46,10 @@ public class StreamMain {
             System.out.println("Poem with additional text");
             return text.replace('L','R');
         });
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
     }
 }
