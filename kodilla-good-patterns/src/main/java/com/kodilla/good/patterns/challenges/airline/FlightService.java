00ab0d66016cde2c78c416aad4893846dd48b8a0
kaczorowski91@gmail.com <kaczorowski91@gmail.com>
2020-01-08 12:26:33 +0100

    task 13.5

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/airline/FlightService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/airline/FlightService.java
@@ -2,7 +2,6 @@ package com.kodilla.good.patterns.challenges.airline;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FlightService {
 
@@ -25,57 +24,50 @@ public class FlightService {
         System.out.println("Available flights from Warsaw:" + flight);
     }
 
-    public void availableFlightToParis(FlightRequest flightRequest){
+    public void availableFlightToParis(FlightRequest flightRequest) {
 
         String flight = flightRequest.getFlightList().stream()
                 .filter(f -> f.getDepartureFlight().equals("Warsaw"))
-                .filter(f ->f.getArrivalFlight().equals("Paris"))
+                .filter(f -> f.getArrivalFlight().equals("Paris"))
                 .map(Flight::toString)
-                .collect(Collectors.joining("","\n",""));
+                .collect(Collectors.joining("", "\n", ""));
 
         System.out.println("Available flight to Paris: " + flight);
     }
 
-    public void availableFlightFromWarsawToParisThroughRome(FlightRequest flightRequest){
+    public void availableFlightFromWarsawToParisThroughRome(FlightRequest flightRequest) {
 
         String flightFrom = flightRequest.getFlightList().stream()
                 .filter(f -> f.getDepartureFlight().equals("Warsaw"))
-                .filter(f ->f.getArrivalFlight().equals("Rome"))
+                .filter(f -> f.getArrivalFlight().equals("Rome"))
                 .map(Flight::toString)
-                .collect(Collectors.joining("","\n",""));
+                .collect(Collectors.joining("", "\n", ""));
 
         String flightTo = flightRequest.getFlightList().stream()
                 .filter(f -> f.getDepartureFlight().equals("Rome"))
-                .filter(f ->f.getArrivalFlight().equals("Paris"))
+                .filter(f -> f.getArrivalFlight().equals("Paris"))
                 .map(Flight::toString)
-                .collect(Collectors.joining("","",""));
+                .collect(Collectors.joining("", "", ""));
 
         System.out.println("Available flight from Warsaw to Paris through Rome : " + flightFrom + flightTo);
     }
 
 
-    public void availableFlightFromWarsawToParisThroughAnywhere(FlightRequest flightRequest){
+    public void availableFlightFromWarsawToParisThroughAnywhere(FlightRequest flightRequest) {
 
-       List<Flight> flightFrom = flightRequest.getFlightList().stream()
+        List<String> flightFrom = flightRequest.getFlightList().stream()
                 .filter(f -> f.getDepartureFlight().equals("Warsaw"))
+                .filter(f -> !f.getArrivalFlight().equals("Paris"))
+                .map(Flight::getArrivalFlight)
                 .collect(Collectors.toList());
 
+      //  List<Flight> flightThrough = flightRequest.getFlightList().stream()
 
-        List<String> flightThrough = flightFrom.stream()
-                .map(Flight::getArrivalFlight)
-               .collect(Collectors.toList());
 
-        flightThrough.forEach(f->f.toUpperCase());
 
-/*        List<Flight> flightTo = flightRequest.getFlightList().stream()
-                .filter(f -> f.getArrivalFlight().equals("Paris"))
-                .filter((f -> f.getDepartureFlight().equals(flightThrough)))
-                .collect(Collectors.toList());*/
 
-        System.out.println("List<Flight> flightFrom " +flightFrom+" List<String> flightThrough " +flightThrough+ " flightTo " );
+        System.out.println("TEST  flightFrom " + flightFrom + "TEST  flightThrough " );
     }
-
-
 
 
 }
