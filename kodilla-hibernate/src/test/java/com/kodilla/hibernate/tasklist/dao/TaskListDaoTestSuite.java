fd517188972dc98cf43a45125fd7ba1dc65a9e7e
kaczorowski91@gmail.com <kaczorowski91@gmail.com>
2020-02-05 11:25:37 +0100

    #Task 17

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -44,25 +44,35 @@ public class TaskListDaoTestSuite {
         //Given
         Task task = new Task("Test: Learn Hibernate", 14);
         Task task2 = new Task("Test: Write some entities", 3);
+
         TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
         TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+
         task.setTaskFinancialDetails(tfd);
         task2.setTaskFinancialDetails(tfd2);
+
         TaskList taskList = new TaskList(LISTNAME, "ToDo tasks");
+
         taskList.getTasks().add(task);
         taskList.getTasks().add(task2);
+
         task.setTaskList(taskList);
         task2.setTaskList(taskList);
 
         //When
         taskListDao.save(taskList);
         int id = taskList.getId();
+        String taskListName = taskList.getTasks().get(0).getDescription();
+        String taskListName2 = taskList.getTasks().get(1).getDescription();
 
         //Then
         Assert.assertNotEquals(0, id);
+        Assert.assertEquals("Test: Learn Hibernate",taskListName);
+        Assert.assertEquals("Test: Write some entities",taskListName2);
 
         //CleanUp
         taskListDao.deleteById(id);
+        taskListDao.delete(taskList);
     }
 
     @Test
